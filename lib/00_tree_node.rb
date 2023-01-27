d0545a6684c331d94b9f9a9bc73ad1dbb6f2d122
wtdavis <wtdavis@wesleyan.edu>
2023-01-27 14:36:17 -0500

    handle nil and parent reassign almost

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,4 +1,7 @@
 class PolyTreeNode
+
+    attr_reader :parent, :children, :value
+
   def initialize(value)
     @parent = nil
     @children = []
@@ -6,12 +9,29 @@ class PolyTreeNode
   end
 
   def parent=(node)
-    if @parent == nil
-      @parent = node
-      node.children << self
+    if node == nil
+        if @parent != nil
+            index = @parent.children.index(self) 
+            @parent.children.delete_at(index)
+        end
+    else
+        if @parent == nil 
+        @parent = node
+        @parent.children << self
+        else
+            index = @parent.children.index(self) 
+            @parent.children.delete_at(index)
+            @parent = node
+            @parent.children << self
+        end
     end
+        
   end
 
+  def inspect
+    "<PTN: parent = #{parent.value}, children = #{children.value}, value = #{value}"
+  end
   
-  attr_reader :parent, :children, :value
+
+
 end
