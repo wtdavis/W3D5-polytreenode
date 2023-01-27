b8c20150eb43814934a71e5cae6b35c9073f5b0a
Olga Borovikova <olga.sleepless@gmail.com>
2023-01-27 14:04:18 -0500

    initialize and setter

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -1,3 +1,17 @@
 class PolyTreeNode
+  def initialize(value)
+    @parent = nil
+    @children = []
+    @value = value
+  end
 
+  def parent=(node)
+    if @parent == nil
+      @parent = node
+      node.children << self
+    end
+  end
+
+  
+  attr_reader :parent, :children, :value
 end
